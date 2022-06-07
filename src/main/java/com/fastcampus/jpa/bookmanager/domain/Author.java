89cd318818c2386553601506b9e72aea0ed10097
RandyYOUN <106806490+RandyYOUN@users.noreply.github.com>
2022-06-07 18:26:32 +0900

    2022-06-07 Office

---
+++ src/main/java/com/fastcampus/jpa/bookmanager/domain/Author.java
@@ -0,0 +1,30 @@
+package com.fastcampus.jpa.bookmanager.domain;
+
+import java.util.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+
+@Entity
+@NoArgsConstructor
+@Data
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class Author extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String country;
+    @ManyToMany
+    @ToString.Exclude
+    private List<Book> books = new ArrayList<>();
+
+    public void addBook(Book... book){
+        Collections.addAll(this.books,book);
+    }
+}
