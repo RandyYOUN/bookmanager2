7671082bd2f5cc6efd3069eba84fea0a3a636f65
RandyYOUN <106806490+RandyYOUN@users.noreply.github.com>
2023-12-31 00:30:18 +0900

    1

--- src/main/java/com/fastcampus/jpa/bookmanager/service/BookService.java
+++ src/main/java/com/fastcampus/jpa/bookmanager/service/BookService.java
@@ -8,38 +8,55 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 @Service
 @RequiredArgsConstructor
 public class BookService {
     private final BookRepository bookRepository;
     private final AuthorRepository authorRepository;
+    private final EntityManager entityManager;
+    private final AuthorService authorService;
 
-    @Transactional(rollbackFor = Exception.class)
+    @Transactional(propagation = Propagation.REQUIRED)
     public void putBookAndAuthor(){
         Book book = new Book();
         book.setName("JPA시작하기");
 
         bookRepository.save(book);
 
-        Author author =  new Author();
-        author.setName("yhd0626");
-        authorRepository.save(author);
+        try{
+            authorService.putAuthor();
+        }catch (RuntimeException e){}
 
-        throw new RuntimeException("오류발생!! rollback!!!!");
+        throw new RuntimeException("오류발생!! transaction은 어떻게 될까요!!!!");
+//
+//        Author author =  new Author();
+//        author.setName("yhd0626");
+//        authorRepository.save(author);
+
+        //throw new RuntimeException("오류발생!! rollback!!!!");
+
     }
 
-    @Transactional(isolation = Isolation.READ_UNCOMMITTED)
+    @Transactional(isolation = Isolation.SERIALIZABLE)
     public void get(Long id){
         System.out.println(">>>>>>>>>>>>"+bookRepository.findById(id));
         System.out.println(">>>>>>>>>>>>"+bookRepository.findAll());
 
+        entityManager.clear();
+
         System.out.println(">>>>>>>>>>>>"+bookRepository.findById(id));
         System.out.println(">>>>>>>>>>>>"+bookRepository.findAll());
 
-        Book book = bookRepository.findById(id).get();
-        book.setName("바뀔까?????????");
-        bookRepository.save(book);
+        bookRepository.update();
+        entityManager.clear();
+
+//        Book book = bookRepository.findById(id).get();
+//        book.setName("바뀔까?????????");
+//        bookRepository.save(book);
     }
 }
